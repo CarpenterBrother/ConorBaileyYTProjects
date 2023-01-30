fa494eb6af57d73076b5238d9e060c1a923c1404
mahdi <mahdihiphopi80@gmail.com>
2023-01-30 04:11:02 +0330

    Added Animated Grid Project

---
+++ Animated-Grid-Smooth-Transitions/js/projects.js
@@ -0,0 +1,85 @@
+let projects = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&w=1920&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
+    title: "Title-one",
+    content: [
+      "this is some test content.",
+      "Lorem ipsum dollar sit amet something antoher",
+    ],
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&w=1920&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
+    title: "Title-two",
+    content: [
+      "this is some test content.",
+      "Lorem ipsum dollar sit amet something antoher",
+    ],
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&w=1920&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
+    title: "Title-three",
+    content: [
+      "this is some test content.",
+      "Lorem ipsum dollar sit amet something antoher",
+    ],
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&w=1920&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
+    title: "Title-four",
+    content: [
+      "this is some test content.",
+      "Lorem ipsum dollar sit amet something antoher",
+    ],
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&w=1920&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
+    title: "Title-five",
+    content: [
+      "this is some test content.",
+      "Lorem ipsum dollar sit amet something antoher",
+    ],
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&w=1920&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
+    title: "Title-six",
+    content: [
+      "this is some test content.",
+      "Lorem ipsum dollar sit amet something antoher",
+    ],
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&w=1920&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
+    title: "Title-seven",
+    content: [
+      "this is some test content.",
+      "Lorem ipsum dollar sit amet something antoher",
+    ],
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&w=1920&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
+    title: "Title-eight",
+    content: [
+      "this is some test content.",
+      "Lorem ipsum dollar sit amet something antoher",
+    ],
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&w=1920&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
+    title: "Title-nine",
+    content: [
+      "this is some test content.",
+      "Lorem ipsum dollar sit amet something antoher",
+    ],
+  },
+];
+
+export { projects };
